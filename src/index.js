9d5fec3b88f2eb2cdd0cd89f6b3d67a56a412ffd
unknown <anass.belhaddad@gmail.com>
2021-09-26 00:59:37 +0100

    Set up route for deleting a user or a task

--- src/index.js
+++ src/index.js
@@ -63,6 +63,18 @@ app.patch("/users/:id",async (req,res)=>{
     }
 })
 
+app.delete("/users/:id",async (req,res)=>{
+    try{ 
+        const user= await User.findByIdAndDelete(req.params.id)
+
+        if (!user) return res.status(404).send()
+
+        res.send(user)
+    }catch(error){
+        res.status(500).send()
+    }
+})
+
 app.post("/tasks", async(req,res)=>{
     const task= new Task(req.body)
 
@@ -115,6 +127,18 @@ app.patch("/tasks/:id", async (req,res)=>{
         res.status(500).send(error)
     }
 
+})
+
+app.delete("/tasks/:id", async(req,res)=>{
+    try{
+        const task= await Task.findOneAndDelete(req.params.id)
+
+        if(!task) return res.status(404).send()
+
+        res.send(task)
+    }catch(e){
+        res.status(500).send()
+    }
 })
 
 
