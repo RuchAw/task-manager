617f48f37b4acf1e87ee0f6771dee14e19353509
unknown <anass.belhaddad@gmail.com>
2021-09-26 00:39:29 +0100

    Adding patch routes to update users & tasks

--- src/index.js
+++ src/index.js
@@ -10,63 +10,110 @@ const port= process.env.PORT || 3000
 
 app.use(express.json())
 
-app.post("/users", (req,res)=>{
+app.post("/users", async(req,res)=>{
     const user= new User(req.body)
-    user.save().then(()=>{
+
+    try{
+        await user.save()
         res.status(201).send(user)
-    }).catch((err)=>{
+    }catch(err){
         res.status(400).send(err)
-    })
+    }
 })
 
-app.get("/users", (req,res)=>{
-    User.find({}).then((users)=>{
+app.get("/users", async (req,res)=>{
+    try{
+        const users= await User.find({})
         res.status(200).send(users)
-    }).catch((err)=>{
+    }catch(err){
         res.status(500).send()
-    }) 
+    }
 })
 
-app.get("/users/:id", (req,res)=>{
+app.get("/users/:id", async(req,res)=>{
     const _id=req.params.id
 
-    User.findById(_id).then((user)=>{
+    try{
+        const user= await User.findById(_id)
+    
         if (!user) return res.status(404).send()
 
         res.status(200).send(user)
-    }).catch((err)=>{
-        res.status(500).send()
-    })
 
+    }catch(err){
+        res.status(500).send()
+    }
 })
 
-app.post("/tasks", (req,res)=>{
+app.patch("/users/:id",async (req,res)=>{
+    const updates= Object.keys(req.body)
+    const allowedUpdates = ["name", "email", "password", "age"]
+    const isValidOperation = updates.every((update)=>allowedUpdates.includes(update))
+
+    if (!isValidOperation) return res.status(400).send({error: "Invalid Updates"})
+
+    try{
+        const user= await User.findByIdAndUpdate(req.params.id,req.body,{ new: true, runValidators: true })
+
+        if (!user) return res.status(404).send()
+
+        res.send(user)
+    }catch(error){
+        res.status(500).send()
+    }
+})
+
+app.post("/tasks", async(req,res)=>{
     const task= new Task(req.body)
-    task.save().then(()=>{
+
+    try{
+        task.save()
         res.status(201).send(task)
-    }).catch((err)=>{
+    }catch(err){
         res.status(400).send(err)
-    })
+    }
 })
 
-app.get("/tasks", (req,res)=>{
-    Task.find({}).then((task)=>{
-        res.status(200).send(task)
-    }).catch((err)=>{
+app.get("/tasks", async(req,res)=>{
+    try{
+        const tasks = await Task.find({})
+        res.status(200).send(tasks)
+    }catch(err){
         res.status(500).send()
-    })
+    }
 })
 
-app.get("/tasks/:id", (req,res)=>{
+app.get("/tasks/:id", async(req,res)=>{
     const _id=req.params.id
 
-    Task.findById(_id).then((task)=>{
+    try{
+        const task= await Task.findById(_id)
+
         if (!task) return res.status(404).send()
 
         res.status(200).send(task)
-    }).catch((err)=>{
+    }catch(err){
         res.status(500).send()
-    })
+    }
+})
+
+app.patch("/tasks/:id", async (req,res)=>{
+
+    const updates= Object.keys(req.body)
+    const allowedUpdates= ["description","completed"]
+    const isValidOperation= updates.every((update)=>allowedUpdates.includes(update))
+
+    if (!isValidOperation) return res.status(400).send({error: "Updates are not allowed !"})
+
+    try{
+        const task = await Task.findByIdAndUpdate(req.params.id,req.body, {new: true, runValidators: true})
+
+        if (!task) return res.status(404).send()
+
+        res.send(task)
+    }catch(error){
+        res.status(500).send(error)
+    }
 
 })
 
