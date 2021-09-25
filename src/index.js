f4df40d1ddc0521cb391b74373a83f6fac65104d
unknown <anass.belhaddad@gmail.com>
2021-09-25 22:53:33 +0100

    Getting a list of Users & Tasks then getting them by their Id

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express= require("express")
 require("./db/mongoose")
 const User= require("./models/user")
 const Task= require("./models/task")
+const { findById } = require("./models/user")
 
 const app = express()
 
@@ -12,19 +13,61 @@ app.use(express.json())
 app.post("/users", (req,res)=>{
     const user= new User(req.body)
     user.save().then(()=>{
-        res.send(user)
-    }).catch((e)=>{
-        res.status(400).send(e)
+        res.status(201).send(user)
+    }).catch((err)=>{
+        res.status(400).send(err)
     })
+})
+
+app.get("/users", (req,res)=>{
+    User.find({}).then((users)=>{
+        res.status(200).send(users)
+    }).catch((err)=>{
+        res.status(500).send()
+    }) 
+})
+
+app.get("/users/:id", (req,res)=>{
+    const _id=req.params.id
+
+    User.findById(_id).then((user)=>{
+        if (!user) return res.status(404).send()
+
+        res.status(200).send(user)
+    }).catch((err)=>{
+        res.status(500).send()
+    })
+
 })
 
 app.post("/tasks", (req,res)=>{
     const task= new Task(req.body)
     task.save().then(()=>{
-        res.send(task)
-    }).catch((e)=>{
-        res.status(400).send(e)
+        res.status(201).send(task)
+    }).catch((err)=>{
+        res.status(400).send(err)
     })
+})
+
+app.get("/tasks", (req,res)=>{
+    Task.find({}).then((task)=>{
+        res.status(200).send(task)
+    }).catch((err)=>{
+        res.status(500).send()
+    })
+})
+
+app.get("/tasks/:id", (req,res)=>{
+    const _id=req.params.id
+
+    Task.findById(_id).then((task)=>{
+        if (!task) return res.status(404).send()
+
+        res.status(200).send(task)
+    }).catch((err)=>{
+        res.status(500).send()
+    })
+
 })
 
 
